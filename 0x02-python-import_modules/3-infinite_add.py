aad4826c08c0f112420129d3f80f4081e7f86cc4
frankmutinda <hlrymutinda@gmail.com.com>
2022-07-02 08:14:50 -0700

    my commit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-def add_arg(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d}".format(n))
-        return
-    else:
-        i = 1
-        add = 0
-        while i <= n:
-            add += int(argv[i])
-            i += 1
-        print("{:d}".format(add))
+from sys import argv
 
 if __name__ == "__main__":
-    import sys
-    add_arg(sys.argv)
+    arg_num = len(argv) - 1
+    if arg_num == 0:
+        print("{}".format(arg_num))
+    else:
+        result = []
+        for i in range(1, arg_num + 1):
+            result.append(int(argv[i]))
+        print("{}".format(sum(result)))
